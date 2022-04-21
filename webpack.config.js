749258772b73aab47e48480171c0e1eba30cce8b
Rafael Pereira <rafaelspereira1@hotmail.com>
2022-04-20 21:34:49 -0300

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "build", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle[hash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new CleanWebpackPlugin(), // Limpa o diretório build antes de cada build
+  ],
+};
