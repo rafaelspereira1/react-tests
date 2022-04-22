7e88e220eec4c52ee5eb8ea1f48da09e940019e1
Rafael Pereira <rafaelspereira1@hotmail.com>
2022-04-22 00:00:08 -0300

    Configure loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,28 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "build", "index.js"),
+  entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "bundle[hash].js",
+    filename: "bundle[fullhash].js",
   },
+  mode: "production",
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
     }),
     new CleanWebpackPlugin(), // Limpa o diretório build antes de cada build
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+  },
 };
